011a827aed93137c24c70c077ffa76b4c5be1b08
Matthew Parker <maddparker@gmail.com>
2020-11-05 10:50:43 -0600

    no message

--- TestingGitGame/Assets/Scripts/NotAGoodSScript.cs
+++ TestingGitGame/Assets/Scripts/NotAGoodSScript.cs
@@ -7,7 +7,7 @@ public class NotAGoodSScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //this is a comment
     }
 
     // Update is called once per frame
